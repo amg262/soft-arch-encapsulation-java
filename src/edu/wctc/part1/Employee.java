4f91b402ec4d49148fe0f9e677de9e3bf6d9cffb
Andrew Gunn <andrewmgunn26@gmail.com>
2022-03-31 14:10:43 -0500

    Update Employee.java

--- src/edu/wctc/part1/Employee.java
+++ src/edu/wctc/part1/Employee.java
@@ -9,14 +9,118 @@ import java.time.LocalDate;
  * needed.
  */
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+    private LocalDate orientationDate;
 
-    public String firstName;
-    public String lastName;
-    public String ssn;
-    public boolean metWithHr;
-    public boolean metDeptStaff;
-    public boolean reviewedDeptPolicies;
-    public boolean movedIn;
-    public String cubeId;
-    public LocalDate orientationDate;
+    public Employee(String firstName, String lastName, String ssn, String cubeId, LocalDate orientationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        this.metWithHr = false;
+        this.metDeptStaff = false;
+        this.reviewedDeptPolicies = false;
+        this.movedIn = false;
+        this.cubeId = cubeId;
+        this.orientationDate = orientationDate;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName != null && firstName.length() >= 1)
+            this.firstName = firstName;
+        else
+            this.firstName = "Sue";
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        //regex = "^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$";
+        this.ssn = ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public LocalDate getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setOrientationDate(LocalDate orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", metWithHr=" + metWithHr +
+                ", metDeptStaff=" + metDeptStaff +
+                ", reviewedDeptPolicies=" + reviewedDeptPolicies +
+                ", movedIn=" + movedIn +
+                ", cubeId='" + cubeId + '\'' +
+                ", orientationDate=" + orientationDate +
+                '}';
+    }
 }
